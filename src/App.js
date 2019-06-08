b778f17488a2b456adf09a0cbe2ea18c5e314731
friendshipark <qls0720@gmail.com>
2019-06-08 16:46:49 +0900

    message

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Aside from './Components/Aside';
-<<<<<<< HEAD
-=======
 
->>>>>>> 3a77cc9fe03589d1c45f3ff6f15b1b2eed5663d5
 import Juyeon from './Juyeon/Juyeon';
 import Woojeong from './Woojeong/Woojeong';
 import Jiwon from './Jiwon/Jiwon';
 import Login from './Jiwon/Login';
 import Signup from './Jiwon/Signup';
-import Haesoo from './Haesoo/Haesoo';
 import Urllist from './Woojeong/Urllist';
 import Timetable from './Woojeong/Timetable';
 
--- src/Woojeong/Urllist.js
+++ src/Woojeong/Urllist.js
@@ -19,35 +19,54 @@ class Urllist extends Component{
 
                
 
-                <li className="menu__list-item">shopping mall top 5</li>
+                <span class = "url-title"> shopping mall top 5</span>
+            
 
-                <li className="menu__list-item"><a className="menu__link" href="https://66girls.co.kr/">1. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="http://hotping.co.kr/">2. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://sonyunara.com/">3. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://www.ggsing.com/">4. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="http://www.kikiko.co.kr/">5. My</a></li>
-                <br></br>
-                <li className="menu__list-item">study web site top 5</li>
-                <li className="menu__list-item"><a className="menu__link" href="https://www.megastudy.net/">1. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://skyedu.conects.com/go3/main">2. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="http://www.etoos.com/home/default.asp">3. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="http://www.mimacstudy.com/main/main.ds">4. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://orbi.kr/">5. My</a></li>
-                <br></br>
-                <li className="menu__list-item">game top 5</li>
-                <li className="menu__list-item"><a className="menu__link" href="https://zigzag.kr/">1. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://zigzag.kr/">2. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://zigzag.kr/">3. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://zigzag.kr/">4. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://zigzag.kr/">5. My</a></li>
-                <br></br>
-                <li className="menu__list-item">beauty top 5</li>
-                <li className="menu__list-item"><a className="menu__link" href="https://www.youtube.com/channel/UCT-_4GqC-yLY1xtTHhwY0hA">1. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://www.youtube.com/user/daddoatv">2. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://www.youtube.com/channel/UC9kmlDcqksaOnCkC_qzGacA">3. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://www.youtube.com/channel/UCZFKgRGh8IPGAp8IvUO_atg">4. My</a></li>
-                <li className="menu__list-item"><a className="menu__link" href="https://www.youtube.com/channel/UCW61wo97HWu_TmmJEOiflrw">5. My</a></li>
+            <ul class = "woojeong-list" style ={{listStyle : "none"}}>
+                <li><a href="https://66girls.co.kr/">1. 66걸즈</a></li>
+                <li><a href="http://hotping.co.kr/">2. My</a></li>
+                <li><a href="https://sonyunara.com/">3. My</a></li>
+                <li><a href="https://www.ggsing.com/">4. My</a></li>
+                <li><a href="http://www.kikiko.co.kr/">5. My</a></li>
+            </ul>
 
+            <br></br>
+
+
+            <span class = "url-title">study web site top 5</span>
+
+            <ul class = "woojeong-list" style ={{listStyle : "none"}}>
+            <li><a href="https://www.megastudy.net/">1. My</a></li>
+            <li><a href="https://skyedu.conects.com/go3/main">2. My</a></li>
+            <li><a href="http://www.etoos.com/home/default.asp">3. My</a></li>
+            <li><a href="http://www.mimacstudy.com/main/main.ds">4. My</a></li>
+            <li><a href="https://orbi.kr/">5. My</a></li>
+            </ul>
+
+            <br></br>
+
+            <span class = "url-title">game top 5</span>
+
+            <ul class = "woojeong-list" style ={{listStyle : "none"}}>
+            <li><a href="https://zigzag.kr/">1. My</a></li>
+            <li><a href="https://zigzag.kr/">2. My</a></li>
+            <li><a href="https://zigzag.kr/">3. My</a></li>
+            <li><a href="https://zigzag.kr/">4. My</a></li>
+            <li><a href="https://zigzag.kr/">5. My</a></li>
+            </ul>
+
+
+            <br></br>
+
+            <span class = "url-title">beauty top 5</span>
+
+            <ul class = "woojeong-list" style ={{listStyle : "none"}}>
+            <li><a href="https://www.youtube.com/channel/UCT-_4GqC-yLY1xtTHhwY0hA">1. My</a></li>
+            <li><a href="https://www.youtube.com/user/daddoatv">2. My</a></li>
+            <li><a href="https://www.youtube.com/channel/UC9kmlDcqksaOnCkC_qzGacA">3. My</a></li>
+            <li><a href="https://www.youtube.com/channel/UCZFKgRGh8IPGAp8IvUO_atg">4. My</a></li>
+            <li><a href="https://www.youtube.com/channel/UCW61wo97HWu_TmmJEOiflrw">5. My</a></li>
+            </ul>
 
                 </Fragment>
                 );
