1af2f58daa58ba84f3898b1aa861d2dbc286b442
friendshipark <qls0720@gmail.com>
2019-06-10 22:32:17 +0900

    우정이가 커밋했어염 뿌우><

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -10,16 +10,23 @@ class Aside extends Component{
                 <h5><a href = "http://localhost:3000/login">login</a>/<a href = "http://localhost:3000/signup">signup</a></h5>
                 <br /><br />
                 <h2>
-                <a href = "http://localhost:3000">home</a>,
+                <a href = "http://localhost:3000">home</a>
                 <br />
-                <a href = "http://localhost:3000/volunteer">volunteer</a>,
-                <a href = "http://localhost:3000/leisure">Leisure</a>,
+                <a href = "http://localhost:3000/volunteer">volunteer</a>
                 <br />
-                <a href = "http://localhost:3000/univ">univ.</a>,
-                <a href = "http://localhost:3000/board">board</a>,
-                <a href = "http://localhost:3000/meal">meal</a>,
+                <a href = "http://localhost:3000/leisure">Leisure</a>
+                <br />
+                <a href = "http://localhost:3000/univ">univ.</a>
+                <br />
+                <a href = "http://localhost:3000/board">board</a>
+                <br />
+                <a href = "http://localhost:3000/meal">meal</a>
                 <br />
                 <a href = "http://localhost:3000/todo">todo</a>
+                <br />
+                <a href = "http://localhost:3000/timetable">timetable</a>
+                <br />
+                <a href = "http://localhost:3000/urllist">urllist</a>
                 </h2>
                 <div className="copywriter">
                     <span>@ajou</span>
