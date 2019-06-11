ca7cd6c177b77f77cd74236f10f5f57b33157e1b
Seyoung <choseyoung45@nate.com>
2019-06-11 22:52:33 +0900

    해수 오늘도 야근 쓰러지면 ....

--- src/Haesoo/calendar.js
+++ src/Haesoo/calendar.js
@@ -1,32 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import Calendar from 'react-calendar';
-import flower from '../shared/image/lunch.png';
 import '../shared/common.css';
- 
+import './hs.css';
+import Wj from '../Woojeong/Woojeong';
+
 class abcd extends Component {
   state = {
     date: new Date(),
   }
- 
+
   onChange = date => this.setState({ date })
- 
+
   render() {
     return (
-        <Fragment>
+      <Fragment>
 
-                <p className="vol_title">Calendar</p>
-                <br /><br /><br />
-
-                <img src={flower} alt="" style={{ position: "absolute", right: 50 }} />
-                
-                <br /><br /><br />
-
-      <div>
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
-      </div>
+        <p className="vol_title">Calendar</p>
+        <div className="haesoo-wrapper">
+          <Calendar className="hs" onChange={this.onChange} value={this.state.date} />
+          <Wj />
+        </div>
       </Fragment>
     );
   }
